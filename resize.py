da95f444187ebb5295b035d4c0930c74451937ce
waze96 <wazee96@gmail.com>
2020-06-15 19:23:21 +0200

    Comments on resize.py

--- resize.py
+++ resize.py
@@ -8,10 +8,19 @@ import scipy.io as sio
 import datetime
 import time
 
+## WARNING, the MAT file is saved in Memory until it is complete. 
+## You need to create small MAT file (MAX 20000 images) or your RAM will be stucked!!
+
+##Path with the original images to obtain LR images.
 data_path_X = "/home/waze/Downloads/DATASETS_RESIZE/PNG8bit/"
+##Path with the original images to obtain HR images.
 data_path_y = "/home/waze/Downloads/DATASETS_RESIZE/DIV2K_train_HR/"
+##Path to save the images in LR.
 resultPath_X = "/home/waze/Downloads/DATASETS_RESIZE/PNG8bit_32/"
+##Path to save the images in HR.
 resultPath_y = "/home/waze/Downloads/DATASETS_RESIZE/DIV2K_train_128/"
+
+##Path to save the MAT file.
 matfilePath = "/home/waze/Downloads/FILE_MAT/"
 
 
@@ -66,11 +75,18 @@ def saveMAT(stop):
 			print('{}% -- {} seconds'.format(i/100, (time.time() - start_time)))
 		if i % stop == 0 and i != 0:
 			break
-	#sio.savemat(matfilePath+'dataset_32to128.mat', {'X':dat, 'y':lbl})
-	sio.savemat(matfilePath+'dataset_32to128.mat', {'X':dat})
+	sio.savemat(matfilePath+'dataset_32to128.mat', {'X':dat, 'y':lbl})
+	#sio.savemat(matfilePath+'dataset_32to128.mat', {'X':dat})
 	now = datetime.datetime.now()
 	print('Finished at {}:{}:{}'.format(now.hour, now.minute, now.second))
 
+
+
+## Size for the LR images: 32x32
 resize(32,32,data_path_X, resultPath_X)
-#resize(128,128,data_path_y, resultPath_y)
+
+## Size for the HR images: 128x128
+resize(128,128,data_path_y, resultPath_y)
+
+## Number of samples in MAT file: 10K
 saveMAT(10000)
